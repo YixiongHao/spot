b52953666b7717846838dea2441fc9b6935e1eb9
Yixiong Hao <124434457+YixiongHao@users.noreply.github.com>
2024-01-07 11:43:28 +1300

    Delete Full_dog_test directory

--- Full_dog_test/BLswing_2D.ino
+++
@@ -1,9 +0,0 @@
-void BLswing_2D(double q1, double q2, double q3)
-{
-  shoulder_ang = map(135, 0, 270, 69, 469);
-  pwm.setPWM(12, 0, shoulder_ang);
-  hip_ang = map(q2, 0, 270, 92, 493);
-  pwm.setPWM(13, 0, hip_ang);
-  knee_ang = map(q3, 0, 270, 92, 482);
-  pwm.setPWM(14, 0, knee_ang); 
-}
--- Full_dog_test/BRswing.ino
+++
@@ -1,9 +0,0 @@
-void BRswing(double q1, double q2, double q3)
-{
-  shoulder_ang = map(q1, 0, 270, 477, 76);
-  pwm.setPWM(0, 0, shoulder_ang);
-  hip_ang = map(q2, 0, 270, 504, 98);
-  pwm.setPWM(1, 0, hip_ang);
-  knee_ang = map(q3, 0, 270, 479, 79);
-  pwm.setPWM(2, 0, knee_ang); 
-}
